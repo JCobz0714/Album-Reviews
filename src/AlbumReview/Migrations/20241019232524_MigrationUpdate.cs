8f02ce2f017c0c3bf51e23ad8fb7b44eb7f0d4d6
J. cob <55332149+JacoboOsorio@users.noreply.github.com>
2024-10-19 18:31:37 -0500

    Updating the database by adding new migrations

---
+++ src/AlbumReview/Migrations/20241019232524_MigrationUpdate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlbumReview.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Rating",
+                table: "Albums",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Review",
+                table: "Albums",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Albums");
+
+            migrationBuilder.DropColumn(
+                name: "Review",
+                table: "Albums");
+        }
+    }
+}
