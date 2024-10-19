8eb25c6f611ba86cf089bee65811e88467a6364c
J. cob <55332149+JacoboOsorio@users.noreply.github.com>
2024-10-19 18:50:53 -0500

    Deleting the favicon

--- src/AlbumReview/obj/Debug/net8.0/AlbumReview.AssemblyInfo.cs
+++ src/AlbumReview/obj/Debug/net8.0/AlbumReview.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlbumReview")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fa0491318d302dd3d8461bbe5184fd6fc97c168")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+663bad9bb0286979de695760f82503bb4d242481")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlbumReview")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlbumReview")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
